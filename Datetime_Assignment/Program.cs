5c31da403774bd9ce2986d2a093fe8e991cda3ed
William_Twomey <48021997+Reilig35@users.noreply.github.com>
2021-09-01 11:56:06 +0100

    Add project files.

---
+++ Datetime_Assignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace Datetime_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now + "\nPlease pick a number:\n");
+            var userInput = Console.ReadLine();
+            using (StreamWriter file = new StreamWriter(@"C:\Users\will2\source\repos\Datetime_Assignment\Datetime_Assignment\log.txt", true))
+            {
+                file.WriteLine(DateTime.Now);
+                file.WriteLine(userInput);
+            }
+
+            var text = File.ReadAllText(@"C:\Users\will2\source\repos\Datetime_Assignment\Datetime_Assignment\log.txt");
+            Console.WriteLine(text);
+        }
+    }
+}
+
